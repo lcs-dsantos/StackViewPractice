5ffa5605cdfa3a7ec3db3d7a43bd48363100d8c6
Santos, Diego <dsantos@lcs-mba3217.lcs.on.ca>
2018-02-27 12:24:12 -0500

    Initial Commit

---
+++ StackViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewPractice
+//
+//  Created by Santos, Diego on 2018-02-27.
+//  Copyright © 2018 Santos, Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
